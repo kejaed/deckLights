a1998612c1c90da4ef0e02a00c4769390081e02d
Ken Edwards <ken@kje.ca>
2016-11-26 23:46:14 -0500

    Adding some python commands to send to arduino from rpi

---
+++ raspi/lightsOn.py
@@ -0,0 +1,16 @@
+import serial
+import sys
+
+ser = serial.Serial('/dev/ttyACM0',57600,timeout=5)
+
+# have a readline here to wait for the arduino to write "---SETUP COMPLETE---" 
+# before we continue with the program
+# note that the arduino will be reset each time we connect
+# can this action be turned off?
+
+hello = ser.readline()
+
+print(hello)
+
+ser.write(b'm1')
+ser.close()
